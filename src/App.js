2b7fef61d04f8c8d5ae94e024dd8042c0673a7b8
павел <pavel-yancen@mail.ru>
2024-10-22 10:43:00 +0300

    Added RandomColor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Accordion from './components/accordian';
+import RandomColor from './components/random-color';
 
 function App() {
   return (
     <div className="App">
       {/* Accordion component */}
       <Accordion />
+
+      {/* Random color component */}
+      <RandomColor/>
     </div>
   );
 }
---
+++ src/components/random-color/index.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+
+export default function RandomColor() {
+  const [typeOfColor, setTypeOfColor] = useState("hex");
+  const [color, setColor] = useState("#000000");
+
+  function randomColorUtility(length) {
+    return Math.floor(Math.random() * length);
+  }
+
+  function handleCreateRandomHexColor() {
+    const hex = [1, 2, 3, 4, 5, 6, "A", "B", "C", "D", "E", "F"];
+    let hexColor = "#";
+
+    for (let i = 0; i < 6; i++) {
+      hexColor += hex[randomColorUtility(hex.length)];
+    }
+
+    setColor(hexColor);
+  }
+
+  function handleCreateRandomRgbColor() {
+    let r = randomColorUtility(256);
+    let g = randomColorUtility(256);
+    let b = randomColorUtility(256);
+
+    setColor(`rgb(${r},${g},${b})`);
+  }
+  useEffect(() => {
+    if (typeOfColor === "rgb") handleCreateRandomRgbColor();
+    else handleCreateRandomHexColor();
+  }, [typeOfColor]);
+
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        background: color,
+      }}
+    >
+      <button onClick={() => setTypeOfColor("hex")}>Random HEX Color</button>
+      <button onClick={() => setTypeOfColor("rgb")}>Random RGB Color</button>
+      <button
+        onClick={
+          typeOfColor === "hex"
+            ? handleCreateRandomHexColor
+            : handleCreateRandomRgbColor
+        }
+      >
+        Generate Random Color
+      </button>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "50px",
+          fontSize: "60px",
+          color: "#fff",
+          flexDirection: "column",
+          gap: "20px",
+        }}
+      >
+        <h3>{typeOfColor === "rgb" ? "RGB Color" : "HEX Color"}</h3>
+        <h1>{color}</h1>
+      </div>
+    </div>
+  );
+}
