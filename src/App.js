9a984eeacb90215552e9823b3a1d39fb36576ba1
павел <pavel-yancen@mail.ru>
2024-10-22 17:49:07 +0300

    Added Image Slider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import './App.css';
-import Accordion from './components/accordian';
-import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
+import "./App.css";
+import Accordion from "./components/accordian";
+import ImageSlider from "./components/image-slider";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 function App() {
   return (
@@ -13,7 +14,14 @@ function App() {
       {/* <RandomColor/> */}
 
       {/* Star rating component */}
-      <StarRating noOfStars={10}/>
+      {/* <StarRating noOfStars={10}/> */}
+
+      {/* Image slider component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }
